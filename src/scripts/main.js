855648289665a78a768af923dac82785439c06f0
mgjeffries <gib.jeffries@gmail.com>
2020-07-15 11:33:51 -0500

    add object creation for plants

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
 import { createPlan } from './plan.js'
+import { createAsparagus } from "./seeds/asparagus.js"
 
 const yearlyPlan = createPlan()
 console.log(yearlyPlan)
 
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
